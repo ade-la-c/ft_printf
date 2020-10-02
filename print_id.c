86888b576d5d6430e6ac387c5668066f9fb6ad6b
Adrian De la colina <ade-la-c@fu-r8-p8.s19.be>
2020-10-02 18:42:42 +0200

    %d et %i en cours

---
+++ print_id.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   print_id.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ade-la-c <ade-la-c@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/10/02 16:41:33 by ade-la-c          #+#    #+#             */
+/*   Updated: 2020/10/02 18:40:52 by ade-la-c         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_printf.h"
+
+void					ft_print_id(t_flag flags, va_list args)
+{
+	
+	return ;
+}
+
+
+
+/*
+la prec. add des 0 avant le nombre
+(0 + width) est equivalent a la precision, a tester d'avantage
+le flag width agit ensuite
+et le flag négatif agit apres la width comme d'habitude ?
+*/
